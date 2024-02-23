8cc5f749e6478aa8213df87fcfb60d53878d8935
Gabriel Ribeiro <gabrielrsilva98@gmail.com>
2024-02-23 00:51:54 +0000

    motivational commit

---
+++ pages/index.jsx
@@ -0,0 +1,3 @@
+export default function HomePage() {
+    return <h1>Continue dando tudo de si e se esforçando pelo que deseja alcançar. Melhor do que dizer que quer algo, é conseguir esse algo.</h1>
+}
