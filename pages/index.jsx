a2e23e363cf0aea321a247fc228111bb52dea4b0
Gabriel Ribeiro <gabrielrsilva98@gmail.com>
2024-03-04 23:10:31 +0000

    chore: add prettier

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,8 @@
 export default function HomePage() {
-    return <h1>Continue dando tudo de si e se esforçando pelo que deseja alcançar. Melhor do que dizer que quer algo, é conseguir esse algo.</h1>
+  return (
+    <h1>
+      Continue dando tudo de si e se esforçando pelo que deseja alcançar. Melhor
+      do que dizer que quer algo, é conseguir esse algo.
+    </h1>
+  );
 }
