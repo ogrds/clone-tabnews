1c28f85314ea92b2d325034663e8dcd8546466cd
Gabriel Ribeiro <gabrielrsilva98@gmail.com>
2024-04-23 03:05:37 +0000

    finish  endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,39 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as sum;");
-  console.log("result", result.rows);
-  response
-    .status(200)
-    .json({ status: "Alunos do curso.dev são acima de média." });
+  const updatedAt = new Date().toISOString();
+
+  const databaseName = process.env.POSTGRES_DB;
+
+  const [
+    databaseVersionResult,
+    databaseMaxConnectionsResult,
+    databaseOpenedConnectionsResult,
+  ] = await Promise.all([
+    database.query("SHOW server_version;"),
+    database.query("SHOW max_connections;"),
+    database.query({
+      text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+      values: [databaseName],
+    }),
+  ]);
+
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
